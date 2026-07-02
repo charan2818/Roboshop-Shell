ac64a31038659543daff9f38fd0d07c62b6dd56e
Dinakara Charan Vasa <vasadinakaracharan@gmail.com>
2026-07-02 10:43:48 +0530

    instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -6,25 +6,25 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do 
-    INSTANCE_ID=$( aws ec2 run-instance \
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instaances[0].InstanceId' \
+    --query 'Instances[0].InstanceId' \
     --output text)
 
-    if [ $instance == "frontend"]; then
+    if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances\
-            --Instance-ids $INSTANCE_ID\
+            --instance-ids $INSTANCE_ID\
             --query 'Reservations[].Instances[].PublicIpAddress'\
             --output text
         )
     else
         IP=$(
             aws ec2 describe-instances\
-            --Instance-ids $INSTANCE_ID\
+            --instance-ids $INSTANCE_ID\
             --query 'Reservations[].Instances[].PrivateIpAddress'\
             --output text
         )
